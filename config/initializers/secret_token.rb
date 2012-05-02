69007c4d49959ee8f34746700efe1d581658af6d
Valeriy Vasin <vv@anahoret.com>
2012-05-02 23:59:31 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarks::Application.config.secret_token = 'edd8bf4b31f4c34f81030477acd4a3738af37ea43b32bdba717a068cc1b2155a018466196125a7ada0ddfa0795c33f5494eeff25046d6acfa792405644193c42'
