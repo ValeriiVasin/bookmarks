ef355fd432b3e6feb8900ca3d83ab4012bd78504
Valeriy Vasin <vv@anahoret.com>
2012-05-06 23:48:19 +0300

    Implement filters-bar module.

---
+++ app/assets/javascripts/modules/filters_bar.js
@@ -0,0 +1,32 @@
+App.createModule('filters-bar', function () {
+  var container,
+      resetFilter,
+      updateFilterState;
+
+  resetFilter = function (data) {
+    var tmpl = App.templates.get('filter');
+    if (!$.isArray(data)) {
+      throw new Error("Filter data should be an array");
+    }
+    container.empty().html(tmpl({ domains: data }));
+  };
+
+  updateFilterState = function () {
+    var data = container.find('input:checkbox:checked').map(function () {
+      return this.value;
+    }).get();
+    App.publish('filter:update', data);
+  };
+
+  return {
+    init: function () {
+      container = $('.filter');
+      container.on('change', 'input:checkbox', updateFilterState);
+      App.subscribe('filter:reset', resetFilter);
+    },
+    destroy: function () {
+      container.off().empty();
+      App.unsubscribe('filter:reset', resetFilter);
+    }
+  };
+});
