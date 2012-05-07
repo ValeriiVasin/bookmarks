b9329d4b3438609262eeddb0dea423b1cdc0d7fe
Valeriy Vasin <vv@anahoret.com>
2012-05-08 02:56:14 +0300

    Implement getDomain() function.

--- app/assets/javascripts/models/bookmark_model.js
+++ app/assets/javascripts/models/bookmark_model.js
@@ -1,1 +1,13 @@
-App.models.bookmark = Backbone.Model.extend({});
+App.models.bookmark = Backbone.Model.extend({
+  getDomain: function () {
+    var url = this.get('url'),
+        matches,
+        domain;
+
+    if ( (matches = url.match(/^(?:https?:\/\/)?([^\/?]+).*$/)) && (domain = matches[1].match(/\.(.{2,4})$/)) ) {
+      return domain ? domain[1] : false;
+    } else {
+      return false;
+    }
+  }
+});
