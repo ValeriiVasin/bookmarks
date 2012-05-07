5e2f2134f8af742f0ea33418331378b54fa8b221
Valeriy Vasin <vv@anahoret.com>
2012-05-07 23:34:26 +0300

    Provide editable fields for bookmarks model.

--- app/assets/javascripts/models/bookmark_model.js
+++ app/assets/javascripts/models/bookmark_model.js
@@ -1,3 +1,13 @@
 App.models.bookmark = Backbone.Model.extend({
-  initialize: function () {}
+  initialize: function () {
+    _.bindAll(this, 'editFields');
+  },
+  editFields: function () {
+    var json = this.toJSON();
+    return {
+      url: json.url,
+      title: json.title,
+      description: json.description
+    };
+  }
 });
