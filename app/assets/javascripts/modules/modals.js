d4eb8273f55ccb2d4f74dbecad3784d39e2e354f
Valeriy Vasin <vv@anahoret.com>
2012-05-08 11:25:58 +0300

    Provide basic validation.

--- app/assets/javascripts/modules/modals.js
+++ app/assets/javascripts/modules/modals.js
@@ -5,9 +5,37 @@ App.createModule('modals', function () {
       newForm,
       editForm,
       defaultFields,
-      modalData;
+      modalData,
+      validate;
 
   defaultFields = { url: '', title: '', description: '' };
+
+  validate = function (data) {
+    var result = true,
+        urlRegExp = /^(?:https?:\/\/)?.+?\.[\w]{2,4}/,
+        generateError;
+    $.each(data, function (key, value) {
+      data[key] = $.trim(value);
+    });
+    generateError = function (field, message) {
+      var el = modal.find(field),
+          controlGroup = el.closest('.control-group');
+      controlGroup.addClass('error').find('.help-block').text(message);
+      el.one('keyup', function () {
+        controlGroup.removeClass('error');
+      });
+      result = false;
+    };
+    if (!data.title) {
+      generateError('#title', "Title can't be blank.");
+    }
+    if (!data.url) {
+      generateError('#url', "URL can't be blank.");
+    } else if (!urlRegExp.test(data.url)) {
+      generateError('#url', "URL is not valid.");
+    }
+    return result;
+  };
 
   createModal = function () {
     modal = $('<div />', { 'class': 'modal' });
@@ -15,19 +43,29 @@ App.createModule('modals', function () {
       App.publish('modal-hide');
     });
     modal.on('click', 'a', function () {
-      var type = $(this).data('type');
+      var type = $(this).data('type'),
+          valid = true,
+          data;
       switch (type) {
         case 'create':
-          App.publish('bookmark:create', modalData());
+          data = modalData();
+          if (valid = validate(data)) {
+            App.publish('bookmark:create', data);
+          }
           break;
         case 'update':
-          App.publish('bookmark:update', modalData());
+          data = modalData();
+          if (valid = validate(data)) {
+            App.publish('bookmark:update', data);
+          }
           break;
         case 'destroy':
           App.publish('bookmark:destroy');
           break;
       }
-      modal.modal('hide');
+      if (valid) {
+        modal.modal('hide');
+      }
       return false;
     });
     $(document.body).append(modal);
