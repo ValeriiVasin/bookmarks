7b220d3974efa80d2ce64c5ce3e6070397473f95
Valeriy Vasin <vv@anahoret.com>
2012-05-10 01:02:15 +0300

    Implement sharing request.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Bookmarks::Application.routes.draw do
   devise_for :users
   resources :bookmarks, :only => [:index, :create, :update, :destroy] do
-    get :share, :on => :member
+    post :share, :on => :member
   end
   root :to => 'pages#index'
 end
