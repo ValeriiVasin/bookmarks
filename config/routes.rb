2e26282a4d1299946b8d6ba02eb0d5b7378098b9
Valeriy Vasin <vv@anahoret.com>
2012-05-10 00:01:00 +0300

    Implement notifier for sharing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Bookmarks::Application.routes.draw do
   devise_for :users
-  resources :bookmarks, :only => [:index, :create, :update, :destroy]
+  resources :bookmarks, :only => [:index, :create, :update, :destroy] do
+    get :share, :on => :member
+  end
   root :to => 'pages#index'
 end
